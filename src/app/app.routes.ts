b0f205f8a1427f8b30c7e2945804e8b63dae7077
walisonbraga <walison.lemos@gmail.com>
2025-03-16 11:51:16 -0500

    add saude facil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './shared/firebase/sign-In/components/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' },
+  { path: 'login', component: SignInComponent, data: { title: 'Login' } }
+  // { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  // { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+  // { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) },
+  // Add more routes here
+];
